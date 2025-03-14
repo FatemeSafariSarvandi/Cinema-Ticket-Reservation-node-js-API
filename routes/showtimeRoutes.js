f7324714abd32ae066de234d9315e79946a3d86d
FatemeSafariSarvandi <safari.sv.fa@gmail.com>
2025-03-14 10:40:21 +0330

    Refactoring showtimeRoutes code

--- routes/showtimeRoutes.js
+++ routes/showtimeRoutes.js
@@ -1,31 +1,74 @@
 const express = require("express");
 const showtime = require("../db/models/showtime");
 const authMiddleware = require("../middleware/authMiddleware");
+const dayjs = require("dayjs");
 
 const router = express.Router();
 
 // Get movie showtimes
 router.get("/:movieId", async (req, res) => {
-    const showtimes = await showtime.findAll({
-        where: { movieId: req.params.movieId },
-    });
-    res.json(showtimes);
+    try {
+        const { movieId } = req.params;
+        if (!movieId || isNaN(Number(movieId))) {
+            return res
+                .status(400)
+                .json({ error: "Invalid movieId. It must be a valid number." });
+        }
+
+        const showtimes = await showtime.findAll({
+            where: { movieId: movieId },
+        });
+        if (!showtimes || showtimes.length === 0) {
+            return res
+                .status(404)
+                .json({ error: "No showtimes found for the given movieId." });
+        }
+        res.json(showtimes);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
 });
 
 // Get movie showtimes by time
 router.get("/time/:time", async (req, res) => {
-    const showtimes = await showtime.findAll({
-        where: { time: req.params.time },
-    });
-    res.json(showtimes);
+    try {
+        const { time } = req.params;
+        if (!time || isNaN(Number(time))) {
+            return res
+                .status(400)
+                .json({ error: "Invalid time. It must be a valid number." });
+        }
+
+        const showtimes = await showtime.findAll({
+            where: { time: time },
+        });
+        if (!showtimes || showtimes.length === 0) {
+            return res
+                .status(404)
+                .json({ error: "No showtimes found for the given time." });
+        }
+        res.json(showtimes);
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
 });
 
 // Get movie showtimes by date
 router.get("/date/:date", async (req, res) => {
-    const showtimes = await showtime.findAll({
-        where: { date: req.params.date },
-    });
-    res.json(showtimes);
+    try {
+        const showtimes = await showtime.findAll({
+            where: { date: req.params.date },
+        });
+        if (!showtimes || showtimes.length === 0) {
+            return res
+                .status(404)
+                .json({ error: "No showtimes found for the given date." });
+        }
+        res.json(showtimes);
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
 });
 
 //Add a showtime (admin only)
@@ -34,7 +77,18 @@ router.post("/", authMiddleware, async (req, res) => {
         return res.status(403).json({ error: "Unauthorized" });
 
     const { movieId, date, time } = req.body;
+
     try {
+        //! When using `new Date` instead of the time value, it returns `invalid`, so I had to use `dayjs`.
+        // const showtimeDateTime = new Date(`${date}T${time}:00:00`);
+        const showtimeDateTime = dayjs(`${date}T${time}:00:00`).toDate();
+        const currentTime = new Date();
+        if (currentTime >= showtimeDateTime) {
+            return res.status(400).json({
+                error: "Cannot create showtime before current",
+            });
+        }
+
         const existingShowtime = await showtime.findOne({
             where: { date, time },
         });
@@ -48,7 +102,7 @@ router.post("/", authMiddleware, async (req, res) => {
         const newShowtime = await showtime.create({ movieId, date, time });
         res.status(201).json(newShowtime);
     } catch (error) {
-        res.status(400).json({ error: error.message });
+        res.status(500).json({ error: "Internal server error" });
     }
 });
 
@@ -56,13 +110,16 @@ router.post("/", authMiddleware, async (req, res) => {
 router.delete("/:id", authMiddleware, async (req, res) => {
     if (req.user.role !== "admin")
         return res.status(403).json({ error: "Unauthorized" });
+    try {
+        const deletedshowtime = await showtime.findByPk(req.params.id);
+        if (!deletedshowtime)
+            return res.status(404).json({ error: "showtime not found" });
 
-    const deletedshowtime = await showtime.findByPk(req.params.id);
-    if (!deletedshowtime)
-        return res.status(404).json({ error: "showtime not found" });
-
-    await deletedshowtime.destroy();
-    res.json({ message: "The showtime was successfully deleted." });
+        await deletedshowtime.destroy();
+        res.json({ message: "The showtime was successfully deleted." });
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
 });
 
 module.exports = router;
