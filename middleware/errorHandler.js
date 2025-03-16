394e984db2965dd87644b85f8aa97e220f71f7b6
FatemeSafariSarvandi <safari.sv.fa@gmail.com>
2025-03-16 11:10:09 +0330

    refactoring errorhandler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,38 +1,3 @@
-// const AppError = require("../utilities/appError");
-// const { ValidationError, UniqueConstraintError } = require("sequelize");
-
-// const errorHandler = (error, req, res, next) => {
-//     console.log(error.message);
-//     console.log(error.name);
-
-//     if (error instanceof UniqueConstraintError) {
-//         return res
-//             .status(409)
-//             .json("A user with this information already exists.");
-//     } else if (error instanceof ValidationError) {
-//         return res.status(500).json("Validation is failed");
-//     } else if (error.name == "SequelizeDatabaseError") {
-//         return res.status(400).json({ message: error.message });
-//     } else if (error.name == "SyntaxError") {
-//         return res.status(400).json({ message: "is not valid JSON" });
-//     } else if (error.name == "TypeError") {
-//         return res
-//             .status(400)
-//             .json({ message: "Cannot read properties of undefined" });
-//     } else if (error instanceof AppError) {
-//         return res
-//             .status(error.statusCode)
-//             .json({ errorCode: error.errorCode, message: error.message });
-//     }
-
-//     res.status(500).json({
-//         errorCode: "INTERNAL_SERVER_ERROR",
-//         message: "An unexpected error occurred. Please try again later.",
-//     });
-// };
-
-// module.exports = errorHandler;
-
 const AppError = require("../utilities/appError");
 const {
     ValidationError,
